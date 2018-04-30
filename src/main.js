c3bef6b3ea4ea10a525c2dd691d48916199886ac
choissi129 <mrchoissi@nate.com>
2018-04-30 17:07:27 +0900

    업데이트

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'expose-loader?$!expose-loader?jQuery!jquery';
+import 'expose-loader?$!expose-loader?jQuery!jquery'
 import {TweenMax, Power2, TimelineLite} from "gsap"
 Vue.config.productionTip = false
 
